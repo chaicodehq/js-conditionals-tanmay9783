4ca5698fd8b3136c74635d4f60b34b514b7dede7
tanmay9783 <tanmayagrawal0412@gmail.com>
2026-02-15 01:29:29 +0530

    Complete conditionals lab

--- src/01-ticket-pricing.js
+++ src/01-ticket-pricing.js
@@ -24,4 +24,12 @@
  */
 export function getTicketPrice(age, isWeekend) {
   // Your code here
+  let price ;
+  if (typeof age !== 'number' || age < 0) return -1;
+  if(age<=12) price=8;
+  else if(age<=17) price=12;
+  else if(age<=59) price =15;
+  else price=10;
+  if(isWeekend) price+=3;
+  return price;
 }
